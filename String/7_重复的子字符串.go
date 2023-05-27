fcb2951a35940f2de6e41222e4f310bbfd1424b8
zongjie233 <840573195@qq.com>
2023-05-27 21:45:14 +0800

    5.27 stack

--- String/7_重复的子字符串.go
+++ String/7_重复的子字符串.go
@@ -4,6 +4,7 @@ package main
 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
 */
 
+// KMP解法
 func repeatedSubstringPattern(s string) bool {
 
 }
---
+++ stackAndqueue/1_用栈实现队列.go
@@ -0,0 +1,49 @@
+package main
+
+type MyQueue struct {
+	stackIn  []int
+	stackOut []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{
+		stackIn:  make([]int, 0),
+		stackOut: make([]int, 0),
+	}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.stackIn = append(this.stackIn, x)
+}
+
+// 如果输出栈的数据为空，则需要将输入栈的数据全部导出，如果非空，则直接使用
+func (this *MyQueue) Pop() int {
+	inLen, outLen := len(this.stackIn), len(this.stackOut)
+	if outLen == 0 {
+		if inLen == 0 {
+			return -1
+		}
+		for i := inLen - 1; i >= 0; i-- {
+			this.stackOut = append(this.stackOut, this.stackIn[i])
+		}
+		this.stackIn = []int{}      //导出后清空
+		outLen = len(this.stackOut) // 更新长度
+
+	}
+	val := this.stackOut[outLen-1]
+	this.stackOut = this.stackOut[:outLen-1]
+	return val
+}
+
+func (this *MyQueue) Peek() int {
+	val := this.Pop()
+	if val == -1 {
+		return -1
+	}
+	this.stackOut = append(this.stackOut, val)
+	return val
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.stackIn) == 0 && len(this.stackOut) == 0
+}
