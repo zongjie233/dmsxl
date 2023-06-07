efaab080719019f745474f0bbdd248c1a634b8d1
zongjie233 <840573195@qq.com>
2023-06-07 14:44:11 +0800

    6.7 二叉树的最大最小深度

---
+++ Tree/6_二叉树的最大深度.go
@@ -0,0 +1,25 @@
+package tree
+
+/*
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+*/
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 后续遍历包括了精简部分
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
---
+++ Tree/7_二叉树的最小深度.go
@@ -0,0 +1,28 @@
+package tree
+
+/*
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+*/
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return min(minDepth(root.Right), minDepth(root.Left)) + 1
+}
