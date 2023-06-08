d1b3246155deb70e060b0dd142a4f958f6f0c606
zongjie233 <840573195@qq.com>
2023-06-08 17:42:14 +0800

    6.8 完全二叉树的节点个数

---
+++ Tree/8_完全二叉树的节点个数.go
@@ -0,0 +1,29 @@
+package tree
+
+/*
+给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+
+完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+
+*/
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftH, rightH := 0, 0
+	leftnode := root.Left
+	rightnode := root.Right
+
+	for leftnode != nil {
+		leftH++
+		leftnode = leftnode.Left
+	}
+	for rightnode != nil {
+		rightH++
+		rightnode = rightnode.Right
+	}
+	if rightH == leftH {
+		return 2
+	}
+	return leftH + rightH + 1
+}
