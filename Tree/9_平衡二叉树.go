964550a8b855f30ed7a0dbabf77730529df1b070
zongjie233 <840573195@qq.com>
2023-06-12 15:20:46 +0800

    6.12 平衡二叉树

---
+++ Tree/9_平衡二叉树.go
@@ -0,0 +1,24 @@
+package tree
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	leftHeight := depth(root.Left)
+	rightHeight := depth(root.Right)
+	return abs(rightHeight-leftHeight) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func depth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(depth(root.Left), depth(root.Right)) + 1
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return 0 - a
+	}
+	return a
+}
